571cf52012bd90e3efd741596dfed46c1967995f
VaishnaviBitla <bitlavaishnavi4@gmail.com>
2025-06-12 21:21:16 +0530

    create automated tests for orderSummary

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,13 +1,9 @@
-import { addToCart, cart, loadFromStorage } from '../../data/cart.js';
+import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
-  beforeEach(() => {
-    cart.length = 0; // Reset shared cart between tests
-    localStorage.clear(); // Optional: clean mock localStorage
-  });
-
   it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'setItem');
+
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -15,28 +11,27 @@ describe('test suite: addToCart', () => {
         deliveryOptionId: '1'
       }]);
     });
-
     loadFromStorage();
-    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
 
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(2);
-    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 
   it('adds a new product to the cart', () => {
     spyOn(localStorage, 'setItem');
-    spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([]); // No items in cart
-    });
 
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([]);
+    });
     loadFromStorage();
-    
+
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
-    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 });
