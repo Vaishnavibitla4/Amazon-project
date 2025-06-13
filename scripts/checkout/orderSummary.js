917532a35cff41af4ddc76379046e9538179e3bb
VaishnaviBitla <bitlavaishnavi4@gmail.com>
2025-06-13 14:19:38 +0530

    Convert products into classes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -44,7 +44,7 @@ export function renderOrderSummary() {
               ${matchingProduct.name}
             </div>
             <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
+              ${matchingProduct.getprice()}
             </div>
             <div class="product-quantity js-product-quantity-${matchingProduct.id}">
               <span>
