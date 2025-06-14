836f6a88af2e9cd940a28b618d434da7a3180358
VaishnaviBitla <bitlavaishnavi4@gmail.com>
2025-06-14 13:52:43 +0530

    Add backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,9 @@
+const xhr = new XMLHttpRequest(); //creates a new HTTP message to send to the backend. message=request
+
+xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+});
+
+xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.send(); //asynchronous code
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js'; //runs all the code inside the file
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
