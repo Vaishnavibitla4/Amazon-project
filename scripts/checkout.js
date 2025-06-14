80c66d2d0fa99f407601afecdb741f85a210513d
VaishnaviBitla <bitlavaishnavi4@gmail.com>
2025-06-14 14:15:01 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js'; //runs all the code inside the file
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
+import { loadProducts} from "../data/products.js";
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
